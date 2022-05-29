d5bde24350c9acae23b8e40922add952f3c5626c
hajjjat2004 <hajjjat2004@gmail.com>
2022-05-30 01:08:43 +0300

    d

--- server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.get('/', (req, res) => res.send('Hello World!') )
-
-app.listen(5000, () => console.log('serve is rnning on port 5000'))
-
